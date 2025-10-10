7cd88c5f97944229bb8ac9dc4307ad284999be87
Anurag Chattopadhyay <78897150+AnChii33@users.noreply.github.com>
2025-10-10 20:11:07 +0530

    Updated AdminDashboard.tsx

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -742,7 +742,7 @@ export function AdminDashboard() {
                   <Pie
                     data={distribution}
                     cx="50%"
-                    cy="45%"
+                    cy="48%"
                     labelLine={false}
                     outerRadius={220} // Increased radius
                     fill="#8884d8"
